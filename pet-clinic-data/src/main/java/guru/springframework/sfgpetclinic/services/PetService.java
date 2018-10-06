0706663ae70c72cc7d6db19cc5e3fc090f4fde48
m1igu <Sporting69>
2018-10-06 18:40:21 +0100

    Add release plugin

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,8 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
-import org.springframework.data.repository.CrudRepository;
 
-public interface PetService extends CrudRepository<Pet, Long> {
+public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VatService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VatService.java
@@ -1,8 +1,7 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vat;
-import org.springframework.data.repository.CrudRepository;
 
-public interface VatService extends CrudRepository<Vat, Long> {
+public interface VatService extends CrudService<Vat, Long> {
 
 }
