c5cc83e1ee4cd1a6f3306d82e2e586158ec58782
m1igu <Sporting69>
2018-10-21 10:14:17 +0100

    Add more stuff

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +15,11 @@ public class VetController {
 	private final VetService vetService;
 
 	@GetMapping
-	public String listVets() {
+	public String listVets( Model model ) {
+
+		model.addAttribute( "vets", vetService.findAll() );
+
+
 		return "vets/index";
 	}
 
