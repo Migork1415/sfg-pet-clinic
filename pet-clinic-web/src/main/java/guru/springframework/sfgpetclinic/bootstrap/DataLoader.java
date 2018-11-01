b74c5702a81afd74abb50f8f694dffcde2f43db6
m1igu <Sporting69>
2018-11-01 13:09:34 +0000

    Add more stuff

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +20,26 @@ public class DataLoader implements CommandLineRunner {
 
 	private final VetService vetService;
 
+	private final PetTypeService petTypeService;
+
 	@Override
 	public void run( String... args ) throws Exception {
+		///////////////////
+		// PET TYPES
+		///////////////////
+		PetType dog = new PetType();
+		dog.setName( "Dog" );
+		PetType savedDogPetType = petTypeService.save( dog );
+
+		PetType cat = new PetType();
+		cat.setName( "Cat" );
+		PetType savedCatPetType = petTypeService.save( cat );
+
+		log.info( "Loaded Pet types..." );
+
+		///////////////////
+		// OWNERS
+		///////////////////
 		Owner owner1 = new Owner();
 		owner1.setFirstName( "Michael" );
 		owner1.setLastName( "Weston" );
@@ -33,6 +53,9 @@ public class DataLoader implements CommandLineRunner {
 
 		log.info( "Loaded Owners..." );
 
+		///////////////////
+		// VETS
+		///////////////////
 		Vet vet1 = new Vet();
 		vet1.setFirstName( "Sam" );
 		vet1.setLastName( "Axe" );
