3100aadc5afbc7d592d96d90f559166bf913e413
m1igu <Sporting69>
2018-11-01 12:48:23 +0000

    Add more stuff

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -2,9 +2,15 @@ package guru.springframework.sfgpetclinic.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
+@NoArgsConstructor
 @EqualsAndHashCode( callSuper = true )
 public class Owner extends Person {
+
+	private Set<Pet> pets;
 
 }
