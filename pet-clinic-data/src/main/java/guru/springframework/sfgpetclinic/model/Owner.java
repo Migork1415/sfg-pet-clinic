c79e44ab268047bd2bf728a65fbedbe28acb1747
m1igu <Sporting69>
2018-11-01 12:54:02 +0000

    Add more stuff

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -11,6 +11,12 @@ import java.util.Set;
 @EqualsAndHashCode( callSuper = true )
 public class Owner extends Person {
 
+	private String address;
+
+	private String city;
+
+	private String telephone;
+
 	private Set<Pet> pets;
 
 }
