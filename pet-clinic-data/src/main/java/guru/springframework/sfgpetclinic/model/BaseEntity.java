8d9ec3a49df326a1271b87eeccb276a52fb7a78c
m1igu <Sporting69>
2018-11-10 15:21:34 +0000

    Add more stuff

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,16 +1,23 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 2441302184792880252L;
 
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
 	private Long id;
 
 }
